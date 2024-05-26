29973a6b46306f2c2527e238396580de93d51aa9
sowmyarao003 <132427018+sowmyarao003@users.noreply.github.com>
2024-05-26 19:29:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ encoder = joblib.load("ordinal_encoder2.joblib")
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # 1: serious injury, 2: Slight injury, 0: Fatal Injury
-
 st.set_page_config(page_title="PREDECTIVE ANALYSIS FOR ACCIDENT PREVENTION AND RESPONSE",
                 page_icon="🚧", layout="wide")
 
 #creating option list for dropdown menu
 options_day = ['Sunday', "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 options_age = ['18-30', '31-50', 'Over 51', 'Unknown', 'Under 18']
+
 # number of vehical involved: range of 1 to 7
 # number of casualties: range of 1 to 8
 # hour of the day: range of 0 to 23
@@ -30,14 +30,10 @@ options_types_collision = ['Vehicle with vehicle collision','Collision with road
                            'Collision with pedestrians','Rollover','Collision with animals',
                            'Unknown','Collision with roadside-parked vehicles','Fall from vehicles',
                            'Other','With Train']
-
 options_sex = ['Male','Female','Unknown']
-
 options_education_level = ['Junior high school','Elementary school','High school',
                            'Unknown','Above high school','Writing & reading','Illiterate']
-
 options_services_year = ['Unknown','2-5yrs','Above 10yr','5-10yrs','1-2yr','Below 1yr']
-
 options_acc_area = ['Other', 'Office areas', 'Residential areas', ' Church areas',
        ' Industrial areas', 'School areas', '  Recreational areas',
        ' Outside rural areas', ' Hospital areas', '  Market areas',
@@ -47,6 +43,7 @@ options_acc_area = ['Other', 'Office areas', 'Residential areas', ' Church areas
 # features list
 features = ['Number_of_vehicles_involved','Number_of_casualties','Hour_of_Day','Type_of_collision','Age_band_of_driver','Sex_of_driver',
        'Educational_level','Service_year_of_vehicle','Day_of_week','Area_accident_occured']
+
 # Give a title to web app using html syntax
 st.markdown("🚧PREDECTIVE ANALYSIS FOR ACCIDENT PREVENTION AND RESPONSE 🚧", unsafe_allow_html=True)
 
@@ -54,7 +51,6 @@ st.markdown("🚧PREDECTIVE ANALYSIS FOR ACCIDENT PREVENTION AND RESPONSE 🚧",
 def main():
        with st.form("road_traffic_severity_form"):
               st.subheader("Please enter the following inputs:")
-              
               No_vehicles = st.slider("Number of vehicles involved:",1,7, value=0, format="%d")
               No_casualties = st.slider("Number of casualities:",1,8, value=0, format="%d")
               Hour = st.slider("Hour of the day:", 0, 23, value=0, format="%d")
@@ -65,8 +61,8 @@ def main():
               service_vehicle = st.selectbox("Service year of vehicle:", options=options_services_year)
               Day_week = st.selectbox("Day of the week:", options=options_day)
               Accident_area = st.selectbox("Area of accident:", options=options_acc_area)
-              
               submit = st.form_submit_button("Predict")
+         
 # encode using ordinal encoder and predict
        if submit:
               input_array = np.array([collision,
@@ -87,11 +83,9 @@ def main():
                      st.write(f"The severity prediction is serious injury⚠")
               else:
                      st.write(f"The severity prediction is slight injury.")
-
 a,b,c = st.columns([0.2,0.6,0.2])
 with b:
   st.image("vllkyt19n98psusds8.jpg", use_column_width=True)
-
 st.subheader("🧭 GUIDELINES:")
 st.text("""   Here we create a web application that takes input from the user and predicts the severity of the accident based on the model that is been deployed.The
 user needs to fill all the required details of the accident in order to predict the severity.""")
